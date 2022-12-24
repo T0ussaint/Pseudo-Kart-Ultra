29a641249c13c7e860be10af8d43cac39ccb5339
T0ussaint <ptussi26@gmail.com>
2022-12-24 13:50:08 -0500

    Driving and Steering Enabled
    basic drive mechanics complete, drifting and boosting variables initialized

--- Pseudo Kart Ultra/Assets/Scripts/KartDriver.cs
+++ Pseudo Kart Ultra/Assets/Scripts/KartDriver.cs
@@ -16,10 +16,27 @@ public class KartDriver : MonoBehaviour
 
     private float[] lastLengths = new float[4], springLengths = new float[4];
     private Vector3[] rayOrigin = new Vector3[4];
+    private Vector3 suspensionForce;
 
     private float springForce, springVel, damperForce;
     private float maxLength, minLength, dim;
-    private Vector3 suspensionForce;
+
+    [Header("Driving & Steering")]
+    [SerializeField] private float maxSpeed;
+    [SerializeField] private float steerConst;
+
+    private Vector3 driveVel, steerVect;
+
+    private float steerDir, steerVal;
+    private float currentSpeed = 0;
+    private float realSpeed;
+
+    [Header("Drifting & Boosting")]
+    [SerializeField] private float minDriftSpeed;
+    [SerializeField] private float centriForce;
+
+    private bool rightDrift, leftDrift, isDrifting;
+    private float driftTime, boostTime, boostSpeed;
 
     private bool isGrounded;
 
@@ -30,16 +47,40 @@ public class KartDriver : MonoBehaviour
 
         dim = sc.radius;
 
-        riseLength = riseLength > dim ? riseLength : dim / 2f + riseLength;
+        riseLength = riseLength >= dim ? riseLength : dim / 2f + riseLength;
         minLength = riseLength - springTravel;
         maxLength = riseLength + springTravel;
+
+        rightDrift = leftDrift = isDrifting = false;
+        boostSpeed = maxSpeed + 20;
     }
 
-    void FixedUpdate(){
+    void FixedUpdate()
+    {
         suspensionPhysics();
+
+        realSpeed = transform.InverseTransformDirection(rb.velocity).z;
+        steerDir = Input.GetAxisRaw("Horizontal");
+
+        steerVal = realSpeed / (realSpeed > 30 ? 4 : 1.5f) * steerDir * steerConst;
+        steerVect = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y + steerVal, transform.eulerAngles.z);
+
+        if(Input.GetKey(KeyCode.UpArrow))
+            currentSpeed = Mathf.Lerp(currentSpeed, maxSpeed, Time.deltaTime / 2f);
+        else if(Input.GetKey(KeyCode.DownArrow))
+            currentSpeed = Mathf.Lerp(currentSpeed, -maxSpeed / 1.75f, 1f * Time.deltaTime);
+        else
+            currentSpeed = Mathf.Lerp(currentSpeed, 0, Time.deltaTime * 1.5f);
+        
+        driveVel = transform.forward * currentSpeed;
+        driveVel.y = rb.velocity.y;
+        rb.velocity = driveVel;
+
+        transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, steerVect, 3f * Time.deltaTime);
     }
 
-    void suspensionPhysics(){
+    void suspensionPhysics()
+    {
         rayOrigin[0] = transform.position + (transform.right + transform.forward) * dim;
         rayOrigin[1] = transform.position - (transform.right - transform.forward) * dim;
         rayOrigin[2] = transform.position + (transform.right - transform.forward) * dim;
@@ -49,9 +90,8 @@ public class KartDriver : MonoBehaviour
         {
             RaycastHit hit;
             Physics.Raycast(rayOrigin[i], -transform.up, out hit, maxLength + wheelRadius);
-            if(hit.collider != null)
+            if (hit.collider != null)
             {
-                Debug.Log(hit.collider.name);
                 lastLengths[i] = springLengths[i];
                 springLengths[i] = Mathf.Clamp(hit.distance - wheelRadius, minLength, maxLength);
 
@@ -62,7 +102,8 @@ public class KartDriver : MonoBehaviour
                 suspensionForce = (springForce + damperForce) * transform.up;
                 rb.AddForceAtPosition(suspensionForce, rayOrigin[i]);
 
-                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.FromToRotation(transform.up * 2f, hit.normal) * transform.rotation, 7.5f * Time.deltaTime);
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.FromToRotation(transform.up * 2f, hit.normal) 
+                    * transform.rotation, 7.5f * Time.deltaTime);
                 isGrounded = true;
             }
             else
